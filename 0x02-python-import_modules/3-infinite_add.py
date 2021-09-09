7c72ec119cfc8d462b1251c0984caad22dc7d6f5
Andres Ortiz <3283@holbertonschool.com>
2021-09-09 10:20:46 -0700

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    a = len(argv)
+    b = 0
+    if a == 1:
+        print("0")
+    else:
+        for i in range(1, a):
+            b += int(argv[i])
+        print("{}".format(b))
