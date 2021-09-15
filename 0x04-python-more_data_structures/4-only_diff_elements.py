e44a7dd04985e40b09435cbf4f57b42e2fad77e3
Andres Ortiz <3283@holbertonschool.com>
2021-09-15 18:15:20 +0000

    0x04.Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,5 +4,5 @@ def only_diff_elements(set_1, set_2):
     for x in set_1:
         for y in set_2:
             if x != y:
-               set_3.append(y)
+                set_3.append(y)
     return set(set_3)
