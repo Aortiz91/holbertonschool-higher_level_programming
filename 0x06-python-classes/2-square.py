cd6ba7282c7eb4e61ed44aafe482afc3e08eda89
Andres Ortiz <3283@holbertonschool.com>
2021-09-29 02:17:04 +0000

    0x06. Python - Classes and Objects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,8 +4,10 @@
 
 class Square():
     """Definition of class"""
+
     def __init__(self, size=0):
         """Initializing self size"""
+
         if (isinstance(size, int) == False):
             raise TypeError ("size must be an integer")
         elif (size < 0):
