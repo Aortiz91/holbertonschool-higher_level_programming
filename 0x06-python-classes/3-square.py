bbefd34850e0530d56afe57c595623d1721b65fe
Andres Ortiz <3283@holbertonschool.com>
2021-09-29 02:28:34 +0000

    0x06. Python - Classes and Objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""Defines a square"""
+
+
 class Square():
+    """Defining class Square"""
     def __init__(self, size=0):
-       if (isinstance(size, int) == False):
-           raise TypeError ("size must be an integer")
-       elif (size < 0):
-           raise ValueError ("size must be >= 0")
-       else:
-            self.__size = size
-            
+        """Initializing self size"""
+        if (isinstance(size, int) == False):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+             self.__size = size
+             
     def area(self):
+        """Defining self area"""
         return (self.__size * self.__size)
