5d78fb9721953491f58e7ea43b32afb067084308
Andres Ortiz <3283@holbertonschool.com>
2021-10-13 02:27:28 +0000

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Will define a class"""
+
+
+class BaseGeometry():
+    """Defines a class BaseGeometry"""
+
+    def area(self):
+        """Public instance method area tjat 
+        is not implemented"""
+
+        raise Exception('area() is not implemented')
