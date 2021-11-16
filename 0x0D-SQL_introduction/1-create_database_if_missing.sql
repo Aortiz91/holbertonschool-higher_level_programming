ebde02beb5b9b63544dde3fe6377ee20ba89b49c
Andres Ortiz <3283@holbertonschool.com>
2021-11-16 10:08:34 -0800

    Added 1-4

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Sript that creates the database hbtn_0c_0 in MySQL server
+create database if not exists hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Sript that deletes a database hbtn_0c_0 in MySQL server
+drop database if exists hbtn_0c_0;
