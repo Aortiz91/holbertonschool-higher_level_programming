8416dae7ce81482c940c5c11dbda3d3b88f1777a
Andres Ortiz <3283@holbertonschool.com>
2021-12-18 12:07:35 -0800

    Added 100

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module that contains the class definition of a State and an instance
+    Base = declarative_base():
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from relationship_city import City, Base
+
+class State(Base):
+    """Class defining state table"""
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, primary_key=True,
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade="all, delete-orphan",
+                          backref='states')
