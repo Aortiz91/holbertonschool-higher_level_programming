3d001c4050bb833bc9f7991f6e1849a942d60321
root <3283@holbertonschool.com>
2022-01-10 01:29:57 +0000

    changed decode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,9 @@
 import urllib.request as request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as f:
+    with request.urlopen('https://intranet.hbtn.io/status') as f:
         website = f.read()
-        print("Body response:")
-        print('\t- type: {}'.format(type(website)))
-        print('\t- content: {}'.format(website))
-        print('\t- utf8 content: {}'.format(website.decode("utf-8")))
+        print('Body response:')
+        print("\t- type: {}".format(type(website)))
+        print("\t- content: {}".format(website))
+        print("\t- utf8 content: {}".format(website.decode('utf-8')))
