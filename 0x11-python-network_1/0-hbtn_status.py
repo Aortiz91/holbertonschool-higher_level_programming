ff543fe454e0baace477573f35877c1349fee97e
root <3283@holbertonschool.com>
2022-01-10 01:15:20 +0000

    Commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status
+"""
+
+
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as f:
+        website = f.read()
+        print("Body response:")
+        print('\t- type: {}'.format(type(website)))
+        print('\t- content: {}'.format(website))
+        print('\t- utf8 content: {}'.format(website.decode()))
