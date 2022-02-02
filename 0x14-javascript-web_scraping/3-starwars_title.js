be9e11f9c1c01bd1aa0af5578374c69b47e984bf
Andy <3283@holbertonschool.com>
2022-02-02 15:12:18 +0000

    semistandard corrections

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,11 +2,11 @@
 // Get the status code
 const request = require('request');
 
-let url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
-	if (error) {
-		console.log(err)
-	} else {
-		console.log(JSON.parse(body).title);
-	}
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
